11ec62640c72366d74303a07c6884d0c9deafec3
ray <254719244@qq.com>
2026-02-02 01:40:53 +0800

    day1: 合并两个有序链表

--- python-template/leetcode/editor/cn/merge-two-sorted-lists.py
+++ python-template/leetcode/editor/cn/merge-two-sorted-lists.py
@@ -21,7 +21,25 @@ from common.node import *
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        pass
+        dummy = ListNode(-1)
+        p = dummy
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                p.next = list1
+                list1 = list1.next
+            else:
+                p.next = list2
+                list2 = list2.next
+            p = p.next
+        
+        p.next = list1 if list1 else list2
+        
+        return dummy.next
+
+
+
+
 # @lc code=end
 
 if __name__ == '__main__':
---
+++ python-template/leetcode/editor/cn/partition-list.py
@@ -0,0 +1,45 @@
+#
+# @lc app=leetcode.cn id=86 lang=python3
+# @lcpr version=30307
+#
+# [86] 分隔链表
+#
+
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from typing import *
+from common.node import *
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        pass
+# @lc code=end
+
+if __name__ == '__main__':
+    solution = Solution()
+    # your test code here
+
+
+
+
+
+#
+# @lcpr case=start
+# [1,4,3,2,5,2]\n3\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [2,1]\n2\n
+# @lcpr case=end
+
+#
+
