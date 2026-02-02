4f694ed6d3b573da187a85c9fe1183fcf82fd5c7
ray <254719244@qq.com>
2026-02-03 01:17:22 +0800

    day2

---
+++ python-template/leetcode/editor/cn/merge-k-sorted-lists.py
@@ -0,0 +1,49 @@
+#
+# @lc app=leetcode.cn id=23 lang=python3
+# @lcpr version=30307
+#
+# [23] 合并 K 个升序链表
+#
+
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from typing import *
+from common.node import *
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        
+# @lc code=end
+
+if __name__ == '__main__':
+    solution = Solution()
+    # your test code here
+
+
+
+
+
+#
+# @lcpr case=start
+# [[1,4,5],[1,3,4],[2,6]]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# []\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [[]]\n
+# @lcpr case=end
+
+#
+
--- python-template/leetcode/editor/cn/partition-list.py
+++ python-template/leetcode/editor/cn/partition-list.py
@@ -21,7 +21,25 @@ from common.node import *
 #         self.next = next
 class Solution:
     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
-        pass
+        dummy1 = ListNode(-1)
+        dummy2 = ListNode(-1)
+        p1 = dummy1
+        p2 = dummy2
+        while head is not None:
+            if head.val < x:
+                p1.next = head
+                p1 = p1.next
+            else:
+                p2.next = head
+                p2 = p2.next
+            head = head.next
+        
+        p2.next = None
+        p1.next = dummy2.next
+        
+        return dummy1.next
+
+        
 # @lc code=end
 
 if __name__ == '__main__':
